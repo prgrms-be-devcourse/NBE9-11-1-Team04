c7e3fa6c3b7c4a6895677cc1b4cbe67f75d0557c
jinwoo <realize2578@gmail.com>
2026-03-20 15:35:58 +0900

    chore:#6 BaseEntity id 변수 타입 Long으로 변경

--- src/main/java/com/back/cafe/global/entity/BaseEntity.java
+++ src/main/java/com/back/cafe/global/entity/BaseEntity.java
@@ -20,7 +20,7 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @CreatedDate
     private LocalDateTime created_at;
