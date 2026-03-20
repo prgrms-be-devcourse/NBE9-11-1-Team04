d03be5eac91fe033b0ad2da2cb23bdbe1136aa73
jinwoo <realize2578@gmail.com>
2026-03-20 14:25:51 +0900

    feat:#6 객체 생성 시 기본적으로 포함해야 할 내용을 담은 추상 클래스 생성 객체 아이디, 생성 시간, 수정 시간을 갖는 추상 클래스 BaseEntity 생성

---
+++ src/main/java/com/back/cafe/global/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.back.cafe.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime modified_at;
+}
