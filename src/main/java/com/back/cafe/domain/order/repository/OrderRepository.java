ecfd0751511d09f7d11dbf0c34340397f3107b5e
YJin33 <ujin3261@gmail.com>
2026-03-23 16:13:09 +0900

    fix:충돌 해결

--- src/main/java/com/back/cafe/domain/order/repository/OrderRepository.java
+++ src/main/java/com/back/cafe/domain/order/repository/OrderRepository.java
@@ -4,15 +4,8 @@ import com.back.cafe.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
-import java.util.Optional;
-/**
- * 주문 엔티티 DB 접근을 담당하는 리포지토리
- */
-=======
 import java.util.List;
 
->>>>>>> 3ff72b8 (feature:#2 주문 시간으로 신규/추가 주문 구분)
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUserId(Long userId);
