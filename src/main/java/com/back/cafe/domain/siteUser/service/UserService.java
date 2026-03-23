06413dfe4ab1365407ce862810d5024358c70eb7
hansol0118 <wall197944@naver.com>
2026-03-24 03:09:42 +0900

    feat:#16 User 다건 조회 추가

--- src/main/java/com/back/cafe/domain/siteUser/service/UserService.java
+++ src/main/java/com/back/cafe/domain/siteUser/service/UserService.java
@@ -1,11 +1,15 @@
 package com.back.cafe.domain.siteUser.service;
 
 
+import com.back.cafe.domain.siteUser.dto.UserDto;
 import com.back.cafe.domain.siteUser.entity.SiteUser;
 import com.back.cafe.domain.siteUser.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +20,11 @@ public class UserService {
         SiteUser siteUser = new SiteUser(email,address,zipCode);
         userRepository.save(siteUser);
         return siteUser;
+    }
+
+    public List<UserDto> findAll() {
+        return userRepository.findAll().stream()
+                .map(UserDto::from)
+                .collect(Collectors.toList());
     }
 }
