63e91bc79f0cd4646be76a75aacf537eed901365
YJin33 <ujin3261@gmail.com>
2026-03-24 16:36:36 +0900

    refactor:#2 created 확인 service단으로 이동

--- src/main/java/com/back/cafe/domain/order/controller/OrderController.java
+++ src/main/java/com/back/cafe/domain/order/controller/OrderController.java
@@ -2,13 +2,16 @@ package com.back.cafe.domain.order.controller;
 
 import com.back.cafe.domain.order.dto.OrderDto;
 import com.back.cafe.domain.order.dto.OrderProductDto;
-import com.back.cafe.domain.order.entity.Order;
+import com.back.cafe.domain.order.dto.OrderServiceResponse;
 import com.back.cafe.domain.order.service.OrderService;
 import com.back.cafe.global.rsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -30,14 +33,13 @@ public class OrderController {
     public RsData<OrderDto> createUpdateOrder(
             @RequestBody @Valid OrderWriteReqBody reqBody
     ){
-        Order order = orderService.doOrder(reqBody.userId, reqBody.orderProductRequests);
-        boolean created = order.getCreatedAt() == order.getModifiedAt();
-        String msg = created?"번 주문이 생성되었습니다.":"번 추가주문이 완료되었습니다.";
-        String resultCode = created?"201-1":"200-1";
+        OrderServiceResponse res = orderService.doOrder(reqBody.userId, reqBody.orderProductRequests);
+        String msg = res.created()?"번 주문이 생성되었습니다.":"번 추가주문이 완료되었습니다.";
+        String resultCode = res.created()?"201-1":"200-1";
         return new RsData<>(
-                "%d%s".formatted(order.getId(),msg),
+                "%d%s".formatted(res.order().getId(),msg),
                 resultCode,
-                new OrderDto(order)
+                new OrderDto(res.order())
         );
     }
 
